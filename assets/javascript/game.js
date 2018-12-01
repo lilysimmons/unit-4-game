f57fa23e0a834759bfb1e1188a7c949e2e5e727b
Lily <44213621+lilysimmons@users.noreply.github.com>
2018-11-30 20:10:59 -0500

    dry code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-
-var wins = 1;
-var losses = 1;
+// global variables
+var wins = 0;
+var losses = 0;
 var counter = 0;
 
 // Links to audio element
@@ -15,7 +15,7 @@ var targetNumber = Math.floor(Math.random() * 120) + 19;
 $("#computerScore").text("Number to meet: " + targetNumber);
 
 
-// each image gets a randomized value attributed to it
+// each image gets a randomized value by targeting id
 $("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
 $("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
 $("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
@@ -28,6 +28,7 @@ var resetGame = function(){
     targetNumber = Math.floor(Math.random() * 120) + 19;
     $("#computerScore").text("Number to meet: " + targetNumber);
 
+    
     $("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
     $("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
     $("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
@@ -38,12 +39,26 @@ var resetGame = function(){
     $("#crystalScore").text("Your collection: " + counter);
 
 }
+
+
+// code for capturing value of each crystal by targeting class 
+$(".gem").on("click", function() {
+    var crystalValue = $(this).attr("value");
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+    $("#crystalScore").text("Your collection: " + counter);
+    score();
+});
+
+
+
 // function to capture the score
 var score = function(){
     if (counter === targetNumber) {
         audioElementWin.play();
         alert("You win!");
-        $("#wins").text("Wins: " + wins++);
+        wins++;
+        $("#wins").text("Wins: " + wins);
         resetGame();
   
       }
@@ -51,157 +66,11 @@ var score = function(){
       else if (counter >= targetNumber) {
         audioElementLose.play();
         alert("You lose!! Please try again.");
-        $("#losses").text("Losses: " + losses++);
+        losses++;
+        $("#losses").text("Losses: " + losses);
         resetGame();
       }
 
 }
 
-// captures value for crystal1
-$("#crystal1").on("click", function() {
-    var crystalValue = ($(this).attr("value"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-    $("#crystalScore").text("Your collection: " + counter);
-    score();
 
-
-
-    
-
-    // if (counter === targetNumber) {
-    //   alert("You win!");
-    //   $("#wins").text("Wins: " + wins++);
-    //   resetGame();
-
-    // }
-
-    // else if (counter >= targetNumber) {
-    //   alert("You lose!! Please try again.");
-    //   $("#losses").text("Losses: " + losses++);
-    //   resetGame();
-    // }
-   
-
-});
-
-// captures value for crystal2
-$("#crystal2").on("click", function() {
-    var crystalValue = ($(this).attr("value"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-    $("#crystalScore").text("Your collection: " + counter);
-    score();
-    
-    
-        
-    
-        // if (counter === targetNumber) {
-        //   alert("You win!");
-        //   $("#wins").text("Wins: " + wins++);
-        //   resetGame();
-        // }
-    
-        // else if (counter >= targetNumber) {
-        //   alert("You lose!! Please try again.");
-        //   $("#losses").text("Losses: " + losses++);
-        //   resetGame();
-        // }
-       
-    
-    });
-
-
-// captures for crystal3
-    $("#crystal3").on("click", function() {
-        var crystalValue = ($(this).attr("value"));
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
-        $("#crystalScore").text("Your collection: " + counter);
-        score();
-        
-        
-            
-        
-            // if (counter === targetNumber) {
-            //   alert("You win!");
-            //   $("#wins").text("Wins: " + wins++);
-            //   resetGame();
-            // }
-        
-            // else if (counter >= targetNumber) {
-            //   alert("You lose!! Please try again.");
-            //   $("#losses").text("Losses: " + losses++);
-            //   resetGame();
-            // }
-           
-        
-        });
-
-// captures value for crystal4
-        $("#crystal4").on("click", function() {
-            var crystalValue = ($(this).attr("value"));
-            crystalValue = parseInt(crystalValue);
-            counter += crystalValue;
-            $("#crystalScore").text("Your collection: " + counter);
-            score();
-            
-            
-                
-            
-                // if (counter === targetNumber) {
-                //   alert("You win!");
-                //   $("#wins").text("Wins: " + wins++);
-                //   resetGame();
-                // }
-            
-                // else if (counter >= targetNumber) {
-                //   alert("You lose!! Please try again.");
-                //   $("#losses").text("Losses: " + losses++);
-                //   resetGame();
-                // }
-               
-            
-            });
-
-
-    
-//  // Gets Link for Theme Song
-//  var audioElement = document.createElement("audio");
-//  audioElement.setAttribute("src", "assets/captainplanet24.mp3");
-
-//  // Theme Button
-//  $(".theme-button").on("click", function() {
-//    audioElement.play();
-//  });
-//  $(".pause-button").on("click", function() {
-//    audioElement.pause();
-//  });
-   
-
-// $("#crystalScore").text("Your score: " + userSum);
-    
-  // var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-// var randomCrystal = crystalValue[Math.floor(Math.random() * crystalValue.length)]
-// for (var i = 0; i < crystalValue.length; i++) {
-//      randomCrystal = crystalValue[Math.floor(Math.random() * crystalValue.length)]
-//     console.log(randomCrystal[i]);
-    
-    // // For each iteration, we will create an imageCrystal
-    //  var imageCrystal = $(".gems");
-
-    //  // First each crystal will be given the class ".crystal-image".
-    //  // This will allow the CSS to take effect.
-    //  imageCrystal.addClass("crystal-image");
-
-    //  // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // // This data attribute will be set equal to the array value.
-    // imageCrystal.attr("data-crystalvalue", crystalValue[i]);
-
-    // // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    // $(".gems").append(imageCrystal);
-
-   
-    // console.log(crystalValue[i]);
-    
-// });
